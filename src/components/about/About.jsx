2a4e754912bd5c9c8b337b150af3df557f95b631
Shijin2k3 <shijinkumar2003@gmail.com>
2025-03-29 11:29:01 +0530

    Initialized About Section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import about from '../../assets/profile.webp'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='bg-black text-white py-20 ' id='about'>
+        <div className=' mx-auto px-8 md:px-16 lg:px-24'>
+            <h1 className='text-4xl font-bold uppercase text-center mb-12'>About Me</h1>
+            <div className='flex flex-col md:flex-row  items-center md:space-x-12'>
+                <img src={about} alt=""
+                className='w-72 h-80 rounded object-cover mb-8 md:mb-0' />
+             <div className='flex-1'>
+                <p className='text-lg mb-8'>I'am a passionate Full Stack Web Developer, I specialize in creating dynamic and responsive web applications 
+                      that deliver exceptional user experiences. With a strong foundation in both front-end technologies like HTML,
+                       CSS, and JavaScript, and back-end frameworks such as Node.js and Express, I am adept at building seamless, scalable solutions.
+                </p>
+                <div className='space-y-4'>
+                  <div className='flex items-center'>
+                      <label htmlFor="htmlandcss" className='w-2/12'>HTML & CSS</label>
+                      <div className='grow bg-gray-800 rounded-full h-2.5'>
+                        <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full
+                         transform transition-transform duration-300 hover:scale-105 w-10/12 cursor-pointer'></div>
+                      </div>
+                  </div>
+                  <div className='flex items-center'>
+                      <label htmlFor="react" className='w-2/12'>React Js</label>
+                      <div className='grow bg-gray-800 rounded-full h-2.5'>
+                        <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full
+                         transform transition-transform duration-300 hover:scale-105 w-9/12 cursor-pointer'></div>
+                      </div> 
+                  </div>
+                  <div className='flex items-center'>
+                      <label htmlFor="node" className='w-2/12'>Node Js</label>
+                      <div className='grow bg-gray-800 rounded-full h-2.5'>
+                        <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full
+                         transform transition-transform duration-300 hover:scale-105 w-10/12 cursor-pointer'></div>
+                      </div> 
+                  </div>
+                  <div className='flex items-center'>
+                      <label htmlFor="next" className='w-2/12'>Next Js</label>
+                      <div className='grow bg-gray-800 rounded-full h-2.5'>
+                        <div className='bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full
+                         transform transition-transform duration-300 hover:scale-105 w-8/12 cursor-pointer'></div>
+                      </div> 
+                  </div>
+                </div>
+              </div>
+           </div>
+        </div>
+    </div>
   )
 }
 
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,10 +3,10 @@ import profile from '../../assets/Hero.jpg'
 
 const Hero = () => {
   return (
-    <div className='bg-black'>
-    <div className='relative w-full h-[120vh] sm:h-[100vh] mt-10 bg-black text-white pt-10'>
-      <div className='w-[90%] xl:w-[80%] m-auto flex flex-wrap pt-5'>
-         <div className='w-full lg:w-1/2 '>
+    <div className='bg-black' id='home'>
+    <div className='relative w-full h-[120vh] sm:h-[100vh] mt-10 bg-black text-white py-20'>
+      <div className='w-[90%] xl:w-[80%] m-auto flex flex-col md:flex-row  items-center md:space-x-12 '>
+         <div className='w-full lg:w-[50%] '>
             <div className='flex flex-col items-center lg:items-start'>
               <h1 className='pb-16 text-4xl font-medium tracking-tight lg:mt-16 lg:text-6xl'>SHIJIN KUMAR</h1>
               <span className='bg-gradient-to-r from-green-300  via-slate-400 to-cyan-400
@@ -18,11 +18,11 @@ const Hero = () => {
                  CSS, and JavaScript, and back-end frameworks such as Node.js and Express, I am adept at building seamless, scalable solutions</p>
             </div>
          </div>
-         <div className='w-full lg:w-1/2 lg:pt-10  lg:p-12 xl:p-15  p-5 '>
-            <div className='flex justify-center'>
-              <img src={profile} alt="profile"  className='rounded-full cursor-pointer overflow-hidden'/>
-            </div>
+         <div className='w-full lg:w-[50%] flex items-center justify-center'>
+            <img src={profile} alt="profile" className='w-85 h-85 object-cover mb-8 md:mb-0 rounded-full' />
          </div>
+
+        
       </div>
       
     </div>
