c1ec3fb2acce0c529ba77263bcb95812e8e73d8d
Shijin2k3 <shijinkumar2003@gmail.com>
2025-04-01 10:46:35 +0530

    Initialized animation in about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import about from '../../assets/profile.webp'
+import { motion } from 'framer-motion'
 
 const About = () => {
   return (
@@ -7,14 +8,15 @@ const About = () => {
         <div className=' mx-auto px-8 md:px-16 lg:px-24'>
             <h1 className='text-4xl font-bold uppercase text-center mb-12'>About Me</h1>
             <div className='flex flex-col lg:flex-row  items-center lg:space-x-12'>
-                <img src={about} alt=""
+                <motion.img src={about} alt="" whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:0.5}}
                 className='w-72 h-80 rounded object-cover mb-8 lg:mb-0' />
              <div className='flex-1'>
-                <p className='text-lg mb-8'>I'am a passionate Full Stack Web Developer, I specialize in creating dynamic and responsive web applications 
+                <motion.p whileInView={{opacity:1,x:0}} initial={{opacity:0,x:100}} transition={{duration:0.5}}
+                className='text-lg mb-8'>I'am a passionate Full Stack Web Developer, I specialize in creating dynamic and responsive web applications 
                       that deliver exceptional user experiences. With a strong foundation in both front-end technologies like HTML,
                        CSS, and JavaScript, and back-end frameworks such as Node.js and Express, I am adept at building seamless, scalable solutions.
-                </p>
-                <div className='space-y-4'>
+                </motion.p>
+                <motion.div className='space-y-4' whileInView={{opacity:1,x:0}} initial={{opacity:0,x:100}} transition={{duration:0.5}}>
                   <div className='flex items-center gap-16 md:gap-0'>
                       <label htmlFor="frontend" className='w-2/12'>Frontend</label>
                       <div className='grow bg-gray-800 rounded-full h-2.5'>
@@ -36,7 +38,7 @@ const About = () => {
                          transform transition-transform duration-300 hover:scale-105 w-9/12 cursor-pointer'></div>
                       </div> 
                   </div>
-               </div>
+               </motion.div>
               </div>
            </div>
         </div>
--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { education } from '../../data/data'
-import { GiH2O } from 'react-icons/gi'
+import { motion } from 'framer-motion'
 
 const Education = () => {
   return (
@@ -10,7 +10,8 @@ const Education = () => {
            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center  gap-8'>
              {
                 education.map((e,id)=>(
-                    <div key={e.id} className='bg-gray-950 px-6 pb-6 rounded-lg hover:shadow-lg transform transition-transform
+                    <motion.div key={e.id} whileInView={{opacity:1,x:0}} initial={{opacity:0,x:100}} transition={{duration:0.5}}
+                    className='bg-gray-950 px-6 pb-6 rounded-lg hover:shadow-lg transform transition-transform
                      duration-300 hover:scale-105 flex flex-col items-center cursor-pointer'>
                         <div>
                         <img src={e.image} alt="" className='mt-8 h-20 w-20 rounded-lg scale-110 overflow-hidden bg-gray-300'/>
@@ -24,7 +25,7 @@ const Education = () => {
                              <h2 className='mt-4 font-bold text-2xl text-transparent bg-clip-text
                              bg-gradient-to-r from-green-400 to-blue-500'>{e.grade}</h2>
                         </div>
-                    </div>
+                    </motion.div>
                 ))
              }
            </div>
